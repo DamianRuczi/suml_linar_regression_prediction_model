75a8add8d78258e0c856f8beb443c00e81302438
s27499 <s27499@pjwstk.edu.pl>
2026-04-11 13:42:03 +0200

    make function more generic, extract file name to the function argument

--- ml_model/linear_regression.py
+++ ml_model/linear_regression.py
@@ -5,12 +5,12 @@ import pickle
 
 print("hello world, here is Ml model")
 
-def train_linear_regression_model(csv_path):
+def train_linear_regression_model(csv_path, pickle_path = "our_model.pkl"):
     df = pd.read_csv(csv_path)
     X = df['x'].values.reshape(-1,1) # values converts it into a numpy array
     y = df['y'].values.reshape(-1,1) # -1 means that calculate the dimension of rows, but have 1 column
     linear_regressor = LinearRegression()
     linear_regressor.fit(X, y)
-    pickle.dump(linear_regressor, open('our_model.pkl', 'wb'))
+    pickle.dump(linear_regressor, open(pickle_path, 'wb'))
 
 train_linear_regression_model('data/10_points.csv')
--- ml_model/main.py
+++ ml_model/main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pickle
 
-def predict_target_value(x):
-    lr_model = pickle.load(open("our_model.pkl", 'rb'))
+def predict_target_value(x, pickle_path = "our_model.pkl"):
+    lr_model = pickle.load(open(pickle_path, 'rb'))
     input = np.array([x]).reshape(-1,1)
     prediction = lr_model.predict(input.reshape(-1,1))
     return prediction[0].reshape(1,-1).flatten()[0]
